c08c80b229af8ee389afa7fffaf7ef5cfc8b9ec7
sammatthews2002 <matthewschrish@gmail.com>
2025-03-18 12:08:47 +0530

    Fixed start command issues

--- run.py
+++ run.py
@@ -97,4 +97,6 @@ def webhook():
         return jsonify({"status": "received"}), 200
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))  # Use cloud-assigned port
+    app.run(host="0.0.0.0", port=port)
+    #app.run(port=8000, debug=True)
